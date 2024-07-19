b4414dd5afc043e87469f5c6f5598d55305ec67d
mdxabu <abdullahfakrudeen2020@gmail.com>
2024-07-19 14:33:43 +0530

    Get lyrics using artist name and song name
    Signed-off-by: mdxabu <abdullahfakrudeen2020@gmail.com>

--- lyrics.py
+++ lyrics.py
@@ -4,4 +4,6 @@ def get_lyrics(artist, title):
     URL = f"https://api.lyrics.ovh/v1/{artist}/{title}"
     response = requests.get(url=URL)
     data = response.json()
-    return data["lyrics"]
+    if 'lyrics' not in data:
+        return "Lyrics not found"
+    return data['lyrics'] 
--- main.py
+++ main.py
@@ -1,20 +1,7 @@
-import yt_dlp
 from lyrics import get_lyrics
 
-def get_video_info(url):
-    ydl_opts = {
-        'format': 'best',
-        'quiet': True,
-    }
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info_dict = ydl.extract_info(url, download=False)
-        return info_dict
-
 if __name__ == "__main__":
-    video_url = input("Enter YouTube video URL: ")
-    video_info = get_video_info(video_url)
-
-    print("Title:", video_info.get('title', 'N/A'))
-    print("Uploader:", video_info.get('uploader', 'N/A'))
-    print(get_lyrics(video_info.get('uploader', 'N/A'), video_info.get('title', 'N/A')))
+    Artist = input("Enter the artist: ")
+    Song = input("Enter the song: ")
+    print(get_lyrics(Artist, Song))
     
