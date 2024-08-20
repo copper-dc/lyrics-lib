6bab6d676a60d81403499a8699ce630ea53cd01d
mdxabu <abdullahfakrudeen2020@gmail.com>
2024-08-20 22:02:36 +0530

    Used jiosaavn api
    Signed-off-by: mdxabu <abdullahfakrudeen2020@gmail.com>

--- lyrics.py
+++ lyrics.py
@@ -1,9 +1,25 @@
+import http
+import json
 import requests
 
-def get_lyrics(artist, title):
-    URL = f"https://api.lyrics.ovh/v1/{artist}/{title}"
-    response = requests.get(url=URL)
-    data = response.json()
-    if 'lyrics' not in data:
-        return "Lyrics not found"
-    return data['lyrics'] 
+# def get_lyrics(artist, title):
+#     URL = f"https://api.lyrics.ovh/v1/{artist}/{title}"
+#     response = requests.get(url=URL)
+#     data = response.json()
+#     if 'lyrics' not in data:
+#         return "Lyrics not found"
+#     return data['lyrics'] 
+
+def get_lyrics(song):
+    conn = http.client.HTTPSConnection("saavn.dev")
+
+    conn.request("GET", "/api/search/songs?query="+song)
+
+    res = conn.getresponse()
+    data = res.read()
+
+    data_dict = json.loads(data.decode("utf-8"))
+
+
+    with open(f"song.json", "w", encoding="utf-8") as file:
+        json.dump(data_dict, file, ensure_ascii=False, indent=4)
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from lyrics import get_lyrics
 
 if __name__ == "__main__":
-    Artist = input("Enter the artist: ")
+    # Artist = input("Enter the artist: ")
     Song = input("Enter the song: ")
-    print(get_lyrics(Artist, Song))
-    
+    get_lyrics(Song)    
